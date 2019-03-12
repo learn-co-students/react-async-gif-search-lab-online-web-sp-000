b80b14e42931d9907d53ca2e8999697e3fd17cd8
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-12 13:44:21 -0400

    rewrite app as class

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import NavBar from './NavBar'
+import NavBar from "./NavBar";
+import GifListContainer from "./GifListContainer";
 
-// the App component should render out the GifListContainer component 
-
-function App() {
+export default class App extends React.Component {
+  render() {
   return (
     <div>
-        < NavBar color='black' title="Giphy Search" />
+      <NavBar color="black" title="Giphy Search" />
+      <GifListContainer />
     </div>
-  )
+  )}
 }
 
-export default App
