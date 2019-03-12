41b41370273f788bf120b81ca78a7627e644a94e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-12 14:22:53 -0400

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 export default class GifList extends Component {
-
   render() {
     return (
       <ul>
         {this.props.gifs.map(gif => (
-          <li>{gif}</li>
+          <li key={this.props.gifs.indexOf(gif)}>
+            <img src={gif} />
+          </li>
         ))}
       </ul>
     );
