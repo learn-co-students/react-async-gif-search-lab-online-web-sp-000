2b43e9333b6b1786a8bb3cc5743998c50cb7bccb
ksummerill <kaitlynsummerill@gmail.com>
2020-12-15 08:17:00 -0800

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,1 +1,17 @@
+import React, { Component } from "react";
 
+class GifList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.data.map((d) => (<li><img src={d.url}/></li>))}
+      </ul>
+    )
+  }
+
+}
