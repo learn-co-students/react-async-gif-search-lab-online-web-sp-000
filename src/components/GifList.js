cef2ffe1cac4c8ae3daea463882511afaae4cc27
CoderJay06 <jaycru06@yahoo.com>
2021-03-01 04:30:35 -0500

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
 class GifList extends Component {
-   render() {
-      return (
-         <div>
-            <ul className='gifs-list'>
-               {console.log(this.props.data)}
-               {this.renderGifs()}
-            </ul>
-         </div>
-      )
-   }
 
    renderGifs = () => {
       return this.props.data.map(gif => {
          return <li><img src={gif.images.original.url} /></li>
       })
+   }
+   
+   render() {
+      return (
+         <div>
+            <ul className='gifs-list'>
+               {this.renderGifs()}
+            </ul>
+         </div>
+      )
    }
 }
 
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -7,16 +7,33 @@ class GifListContainer extends Component {
       data: []
    }
 
-   render() {
-      return (
-         <div>
-            <GifList data={this.state.data}  />
-            <GifSearch handleOnSubmit={this.handleSubmit} />
-         </div>
-      )
+   componentDidMount() {
+      this.fetchGifs()
    }
 
-   componentDidMount() {
+   handleSubmit = (event, value) => {
+      event.preventDefault()
+      this.queryGif(value)
+      // this.fetchGifs()
+   }
+
+   queryGif = value => {
+      const gifDataUrl = `https://api.giphy.com/v1/gifs/` +
+      `search?q=${value}&api_key=dc6zaTOxFJmzC&rating=g`
+      fetch(gifDataUrl)
+         .then(res => res.json())
+         .then(gif => {
+            // debugger
+            console.log(gif.data)
+            this.setState({
+               data: [gif.data[0], 
+                      gif.data[1], 
+                      gif.data[2]]
+            })
+         })
+   }
+
+   fetchGifs = () => {
       const gifDataUrl = 'https://api.giphy.com/v1/gifs/' +
          'search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g'
       fetch(gifDataUrl)
@@ -28,7 +45,16 @@ class GifListContainer extends Component {
                       gifs.data[2]]
             })
          })
+   }
 
+   render() {
+      return (
+         <div>
+            <GifSearch handleOnSubmit={this.handleSubmit} />
+            <GifList data={this.state.data}  />
+            {/* <GifSearch handleOnSubmit={this.handleSubmit} /> */}
+         </div>
+      )
    }
 }
 
